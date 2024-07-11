ba3aee80b82f7894ffa16df92fb1b877cd0d0752
mennosc <47506398+mennosc@users.noreply.github.com>
2024-07-11 15:26:55 +0200

    Refactoring

--- EK-tracker/Program.cs
+++ EK-tracker/Program.cs
@@ -14,11 +14,11 @@ namespace EK_tracker
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-            // Add services to the container.
             builder.Services.AddControllersWithViews();
             builder.Services.AddHttpClient();
-
+            builder.Services.AddDbContext<UserDbContext>(options => options.UseSqlServer(connectionString));
             builder.Services.AddAuthentication("UserCookie").AddCookie("UserCookie", options =>
             {
                 options.Cookie.Name = "UserCookie";
@@ -26,16 +26,12 @@ namespace EK_tracker
                 options.AccessDeniedPath = "/AccessDenied";
             });
 
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-            builder.Services.AddDbContext<UserDbContext>(options => options.UseSqlServer(connectionString));
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
