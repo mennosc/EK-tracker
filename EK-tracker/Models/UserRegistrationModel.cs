4efbc1c933298705b829745737b9f097009e67f8
mennosc <47506398+mennosc@users.noreply.github.com>
2024-07-11 13:45:34 +0200

    Improved Register styles and form validation

--- EK-tracker/Models/UserRegistrationModel.cs
+++ EK-tracker/Models/UserRegistrationModel.cs
@@ -6,13 +6,16 @@ namespace EK_tracker.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string? UserName { get; set; }
-        
+        [Required]
         public string? FirstName { get; set; }
-        
+        [Required]
         public string? LastName { get; set; }
-        
+        [Required]
+        [EmailAddress]
         public string? Email { get; set; }
+        [Required]
         public string? Password { get; set; }
     }
 }
